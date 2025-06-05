0b12d9b19bb4818b718259e5100154a57a010ccd
Jon-Lemmon <luiz.gontijo@ufv.br>
2025-06-05 11:14:09 -0300

    Atualizando SQL e Banco
    Na hora de fazer o merge com a branch pos-entrega-1, houve um problema, e esses arquivos não foram devidamente atualizados.

--- Servidor_BD/db/caldeirao.sql
+++ Servidor_BD/db/caldeirao.sql
@@ -35,15 +35,14 @@ CREATE TABLE IF NOT EXISTS "anuncio" (
 DROP TABLE IF EXISTS "pedido";
 CREATE TABLE IF NOT EXISTS "pedido" (
 	"id_pedido"	INTEGER NOT NULL,
-	"id_produto"	INTEGER NOT NULL,
+	"id_anuncio"	INTEGER NOT NULL,
 	"id_endereco"	INTEGER NOT NULL,
 	"quantidade_pedido"	INTEGER NOT NULL,
-	"preco_anuncio"	NUMERIC NOT NULL,
 	"data_pedido"	INTEGER NOT NULL,
 	"confirmacao_pedido"	INTEGER NOT NULL DEFAULT 0,
-	PRIMARY KEY("id_pedido" AUTOINCREMENT),
 	FOREIGN KEY("id_endereco") REFERENCES "endereco"("id_endereco"),
-	FOREIGN KEY("id_produto") REFERENCES "produto"("id_produto")
+	FOREIGN KEY("id_anuncio") REFERENCES "anuncio"("id_anuncio"),
+	PRIMARY KEY("id_pedido" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "produto";
 CREATE TABLE IF NOT EXISTS "produto" (
@@ -69,4 +68,36 @@ CREATE TABLE IF NOT EXISTS "loja" (
 	PRIMARY KEY("id_loja" AUTOINCREMENT),
 	FOREIGN KEY("id_usuario") REFERENCES "usuario"("id_usuario")
 );
+DROP TRIGGER IF EXISTS "on_criar_pedido";
+CREATE TRIGGER on_criar_pedido
+AFTER INSERT ON pedido
+FOR EACH ROW
+BEGIN
+    -- 1. Subtrair a quantidade do anúncio
+    UPDATE anuncio
+    SET quantidade_produto = quantidade_produto - NEW.quantidade_pedido
+    WHERE id_anuncio = NEW.id_anuncio;
+
+    -- 2. Pausar o anúncio se a quantidade chegar a 0 ou menos
+    UPDATE anuncio
+    SET pausado = 1
+    WHERE id_anuncio = NEW.id_anuncio
+      AND quantidade_produto <= 0;
+END;
+DROP TRIGGER IF EXISTS "on_apagar_pedido";
+CREATE TRIGGER on_apagar_pedido
+AFTER DELETE ON pedido
+FOR EACH ROW
+BEGIN
+    -- 1. Subtrair a quantidade do anúncio
+    UPDATE anuncio
+    SET quantidade_produto = quantidade_produto + OLD.quantidade_pedido
+    WHERE id_anuncio = OLD.id_anuncio;
+
+    -- 2. Pausar o anúncio se a quantidade chegar a 0 ou menos
+    UPDATE anuncio
+    SET pausado = 0
+    WHERE id_anuncio = OLD.id_anuncio
+      AND quantidade_produto > 0;
+END;
 COMMIT;
